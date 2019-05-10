8e5e251b92a07f3d0a9af5ae4b84d97684a51c06
Sharath G Pai <sharath@cliqz.com>
2019-05-10 15:24:02 +0200

    Split Tests per Flavor and Enable Video Record for CI

--- scripts/azure-scripts/launch-emu.sh
+++ scripts/azure-scripts/launch-emu.sh
@@ -1,13 +1,13 @@
 echo "*** Azure Script - Launch Emulator (Also Install App for iOS) ***"
-export DEV_NAME="Nexus5Emu"
+export DEV_NAME="Nexus4Emu"
 echo "DEV_NAME=${DEV_NAME}"
-$ANDROID_HOME/tools/bin/avdmanager create avd --device "Nexus 5" --package "${IMG_NAME}" --abi google_apis/x86 --name "${DEV_NAME}"
+$ANDROID_HOME/tools/bin/avdmanager create avd --device "Nexus 4" --package "${IMG_NAME}" --abi google_apis/x86 --name "${DEV_NAME}"
 $ANDROID_HOME/emulator/emulator -avd $DEV_NAME &
 echo $! >> emu.pid
 echo "Started Emu"
 emuStatus="$(adb shell getprop sys.boot_completed)"
-while [ $emuStatus != "1" ]; do
-    echo $emuStatus
-    emuStatus="$(adb shell getprop sys.boot_completed)"
-done
+#while [ "${emuStatus}" != "1" ]; do
+#    echo "${emuStatus}"
+#    emuStatus="$(adb shell getprop sys.boot_completed)"
+#done
 sleep 60
