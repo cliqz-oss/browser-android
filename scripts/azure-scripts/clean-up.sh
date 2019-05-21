3be6e855ef7b257bcb25d1903ed359f4cc677408
Sharath G Pai <sharath@cliqz.com>
2019-05-21 10:12:26 +0200

    Refactor Azure-Pipelines.yml and remove Scripts

--- scripts/azure-scripts/clean-up.sh
+++
@@ -1,5 +0,0 @@
-echo "*** Stopping Emulator/Simulator ***"
-kill -9 $(cat logcat.pid) || true
-adb kill-server
-kill -9 $(cat emu.pid)
-echo "*** DONE ***"
--- scripts/azure-scripts/copy-builds.sh
+++
@@ -1,7 +0,0 @@
-echo "*** Get FAT Builds for the Flavors ***"
-declare -a flavors=("lumen" "cliqz")
-mkdir builds
-for flavor in "${flavors[@]}"
-do
-    cp app/build/outputs/apk/${flavor}Fat/debug/app-${flavor}-fat-debug.apk builds/${flavor}.apk || true
-done
