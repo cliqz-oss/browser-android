0381d6e45ba726c2d1d58592737e507b3ee8665f
Stefano Pacifici <stefano.pacifici@gmail.com>
2019-05-21 18:37:47 +0200

    AP-251 removes Cliqz icon from the search bar (#63)

---
+++ app/src/cliqz/java/com/cliqz/browser/widget/AutocompleteBackIcon.java
@@ -0,0 +1,33 @@
+package com.cliqz.browser.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.support.annotation.NonNull;
+import android.support.graphics.drawable.VectorDrawableCompat;
+
+import com.cliqz.browser.R;
+
+class AutocompleteBackIcon extends LayerDrawable {
+    private AutocompleteBackIcon(Drawable[] drawables) {
+        super(drawables);
+    }
+
+    public static Drawable create(@NonNull Context context) {
+        final Resources resources = context.getResources();
+        final Drawable backDrawable = VectorDrawableCompat.create(
+                resources, R.drawable.ic_action_back, null);
+        final Drawable logoDrawable = VectorDrawableCompat.create(
+                resources, R.drawable.ic_cliqz_search, null);
+        assert backDrawable != null;
+        assert logoDrawable != null;
+        final int backWidth = backDrawable.getIntrinsicWidth();
+        final int logoWidth = logoDrawable.getIntrinsicWidth();
+        final AutocompleteBackIcon out =
+                new AutocompleteBackIcon(new Drawable[] { backDrawable, logoDrawable });
+        out.setLayerInset(0, 0, 0, logoWidth, 0);
+        out.setLayerInset(1, backWidth, 0, 0, 0);
+        return out;
+    }
+}
