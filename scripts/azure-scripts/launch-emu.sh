36822546b6b44e1a2d0fbc3d78453f52196f8aeb
Krzysztof Jan Modras <chrmod@chrmod.net>
2019-05-24 12:40:19 +0200

    CI: start Android emulator with EN locales (#68)

--- scripts/azure-scripts/launch-emu.sh
+++ scripts/azure-scripts/launch-emu.sh
@@ -2,7 +2,7 @@ echo "*** Azure Script - Launch Emulator (Also Install App for iOS) ***"
 export DEV_NAME="Nexus4Emu"
 echo "DEV_NAME=${DEV_NAME}"
 $ANDROID_HOME/tools/bin/avdmanager create avd --device "Nexus 4" --package "${IMG_NAME}" --abi google_apis/x86 --name "${DEV_NAME}"
-$ANDROID_HOME/emulator/emulator -avd $DEV_NAME &
+$ANDROID_HOME/emulator/emulator -avd $DEV_NAME -prop persist.sys.language=en -prop persist.sys.country=GB &
 echo $! >> emu.pid
 echo "Started Emu"
 emuStatus="$(adb shell getprop sys.boot_completed)"
