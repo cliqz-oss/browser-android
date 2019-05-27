347203f7569667cd970039ddc2f7a569a2bcee1c
Sharath <sharath@cliqz.com>
2019-05-27 12:21:58 +0200

    Refactor Azure-Pipelines.yml and remove Scripts (#60)
    * Refactor Azure-Pipelines.yml and remove Scripts
    
    * Disable Lumen Tests for now and fix for some tests

--- scripts/azure-scripts/videos-screenshots.sh
+++
@@ -1,5 +0,0 @@
-echo "*** Get Videos and Screenshots ***"
-kill -9 $(cat video.pid) || true
-$ANDROID_HOME/platform-tools/adb pull /mnt/sdcard/DCIM/ testresults/ || true
-#$ANDROID_HOME/platform-tools/adb pull /mnt/sdcard/test-screenshots testresults/ || true
-echo "*** Done ***"
