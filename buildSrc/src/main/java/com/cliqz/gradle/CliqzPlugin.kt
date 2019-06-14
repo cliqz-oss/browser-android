ef31fd95cc47d7d664a5098c0251cde3943e5c40
Stefano Pacifici <stefano.pacifici@gmail.com>
2019-06-14 14:40:55 +0200

    Initial buildSrc support

--- buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
+++ buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
@@ -1,77 +1,10 @@
 package com.cliqz.gradle
 
-import com.android.build.OutputFile
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.api.ApkVariantOutput
-import com.android.build.gradle.api.ApplicationVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-@Suppress("unused")
 class CliqzPlugin: Plugin<Project> {
     override fun apply(project: Project) {
-        // TODO: Can this be moved to afterEvaluate too?
-        val android = project.property("android") as AppExtension
-        android.productFlavors.names.forEach {
-            android.defaultConfig.apply {
-                val upperName = it.toUpperCase()
-                buildConfigField("String", "FLAVOR_$upperName", "\"$it\"")
-                buildConfigField("boolean", "IS_$upperName", "FLAVOR.equals(FLAVOR_$upperName)")
-                buildConfigField("boolean", "IS_NOT_$upperName", "!IS_$upperName")
-            }
-        }
-
-        project.afterEvaluate {
-            android.applicationVariants.forEach { variant ->
-                setVersionCode(variant)
-                createCliqzConfigTasks(project, variant)
-            }
-        }
-    }
-
-    private fun createCliqzConfigTasks(project: Project, variant: ApplicationVariant) {
-        val buildConfigProvider = variant.generateBuildConfigProvider
-        val javaCompileProvider = variant.javaCompileProvider
-        // val sourceOutdir = buildConfig.sourceOutputDir
-        val taskName = "generate${variant.name.capitalize()}CliqzConfig"
-        // val packageName = buildConfig.buildConfigPackageName
-        val builtType = variant.buildType.name
-        // This generate possible config names to look for in the json
-        val flavorNames = variant.productFlavors.fold(emptySet<String>()) { acc, productFlavor ->
-            acc + (listOf(productFlavor.name, "${productFlavor.name}${builtType.capitalize()}", builtType))
-        }
-
-        val taskProvider = project.tasks.register(taskName, CliqzConfigTask::class.java, buildConfigProvider, flavorNames)
-        taskProvider.configure {
-            it.dependsOn(buildConfigProvider)
-        }
-        project.tasks.named("compile${variant.name.capitalize()}Kotlin").configure {
-            it.dependsOn(taskProvider)
-        }
-        javaCompileProvider.configure {
-            it.dependsOn(taskProvider)
-        }
-    }
-
-    private fun setVersionCode(variant: ApplicationVariant) {
-        val buildNumber = System.getenv("BUILD_NUMBER")?.toIntOrNull()
-        if (buildNumber == null) return;
-        val versionCode = 150 + buildNumber
-        val apiVersion = variant.productFlavors[0].versionCode
-        variant.outputs.forEach { output ->
-            val abi = output.filters.find { it.filterType == OutputFile.ABI }
-            abi?.let {
-                val abiVersion = ABI_CODES[it.identifier]
-                (output as ApkVariantOutput).versionCodeOverride =
-                        versionCode * 100 + apiVersion * 10 + abiVersion!!
-            }
-        }
-    }
-
-
-
-    companion object {
-        private val ABI_CODES = mapOf(
-                "armeabi-v7a" to 2, "arm64-v8a" to 4, "x86" to 6, "x86_64" to 8)
+        System.out.println(project::class.toString())
     }
 }
