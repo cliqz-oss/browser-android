ee7f14fee40a4422384e002141291eacc0cd3e95
Stefano Pacifici <stefano.pacifici@gmail.com>
2019-06-24 17:17:01 +0200

    Removing scripts/CliqzConfig.gradle

--- buildSrc/src/main/java/com/cliqz/gradle/CliqzConfigTask.kt
+++ buildSrc/src/main/java/com/cliqz/gradle/CliqzConfigTask.kt
@@ -6,8 +6,8 @@ import com.google.gson.reflect.TypeToken
 import com.squareup.javawriter.JavaWriter
 import org.gradle.api.DefaultTask
 import org.gradle.api.logging.Logger
-import org.gradle.api.tasks.*
-import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskProvider
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
@@ -21,33 +21,12 @@ open class CliqzConfigTask @Inject constructor(
         val buildConfigProvider: TaskProvider<GenerateBuildConfig>,
         val flavorNames: Set<String>) : DefaultTask() {
 
-    val inputFile: File? by lazy {
-        val inFile = project.file(CLIQZ_CONFIG_JSON)
-        if (inFile.isFile) inFile else null
-    }
-        @Optional @InputFile get
-
-    private val buildConfigTask: GenerateBuildConfig by lazy { buildConfigProvider.get() }
-    private val packageName: String by lazy { buildConfigTask.buildConfigPackageName }
-
-    val outputDirectory: File by lazy {
-        File(buildConfigTask.sourceOutputDir, packageName.replace('.', File.separatorChar))
-    }
-        @OutputDirectory get
-
-    val outputFile: File by lazy {
-        File(outputDirectory, "CliqzConfig.java")
-    }
-        @OutputFile get
-
     @TaskAction
     fun generateCliqzConfig() {
         // Read the json file
+        val configFile = project.file(CLIQZ_CONFIG_JSON)
         val type = MapTypeToken().type
-        val config = if (inputFile == null) {
-            logger.warn("Missing $CLIQZ_CONFIG_JSON")
-            Config()
-        } else FileReader(inputFile).use {
+        val config = FileReader(configFile).use {
             val configs = Gson().fromJson<Map<String, Config>>(it, type)
             val defaultConfig = configs[DEFAULT_CONFIG_KEY] ?: Config()
             val mergedConfig = configs.filter { (key, _) ->
@@ -58,8 +37,12 @@ open class CliqzConfigTask @Inject constructor(
             defaultConfig.merge(mergedConfig)
             return@use defaultConfig
         }
-
-        FileWriter(outputFile).use { writer ->
+        val builConfigTask = buildConfigProvider.get()
+        val generatedSourceDir = builConfigTask.sourceOutputDir
+        val packageName = builConfigTask.buildConfigPackageName
+        val outDir = File(generatedSourceDir, packageName.replace('.', File.separatorChar))
+        val outFile = File(outDir, "CliqzConfig.java")
+        FileWriter(outFile).use { writer ->
             val javaWriter = JavaWriter(writer)
             javaWriter.emitJavadoc("Auto generated, do not edit")
                     .emitPackage(packageName)
@@ -93,8 +76,6 @@ open class CliqzConfigTask @Inject constructor(
 }
 private class MapTypeToken: TypeToken<Map<String, Config>>()
 
-// All the properties are only used via reflection, so they are "unused"
-@Suppress("unused")
 class Config {
     var amazonAccountID = ""
     var amazonAuthRoleARN = ""
@@ -102,38 +83,26 @@ class Config {
     var amazonIdentityPoolID = ""
     var amazonApplicationARN = ""
     var amazonSnsTopics = mutableListOf<String>()
-    var vpnApiKey = ""
-    var revenuecatApiKey = ""
-    var sentryToken = ""
-    var credentialUrl = ""
 
-    @Suppress("UNCHECKED_CAST")
     fun merge(other: Config, logger: Logger? = null): Config {
-        Config::class.declaredMemberProperties.forEach { member ->
-            when (member.returnType.classifier) {
-                String::class -> mergeStringProperty(logger, other, member as KMutableProperty1<Config, String>)
-                MutableList::class -> mergeListProperty(other, member as KMutableProperty1<Config, List<String>>)
-                else -> logger?.error("Can't merge property ${member.name}")
-            }
-        }
+        amazonAccountID = mergeProperty(logger, other, Config::amazonAccountID)
+        amazonAuthRoleARN = mergeProperty(logger, other, Config::amazonAuthRoleARN)
+        amazonUnauthRoleARN = mergeProperty(logger, other, Config::amazonUnauthRoleARN)
+        amazonIdentityPoolID = mergeProperty(logger, other, Config::amazonIdentityPoolID)
+        amazonApplicationARN = mergeProperty(logger, other, Config::amazonApplicationARN)
+        val topics = amazonSnsTopics.toMutableSet()
+        topics.addAll(other.amazonSnsTopics)
+        amazonSnsTopics = topics.toMutableList()
         return this
     }
 
-    private fun mergeListProperty(other: Config, property: KMutableProperty1<Config, List<String>>) {
-        val topics = property.get(this).toMutableSet()
-        topics.addAll(property.get(other))
-        property.set(this, topics.toMutableList())
-    }
-
-    private fun mergeStringProperty(logger: Logger?, other: Config, property: KMutableProperty1<Config, String>) {
+    private fun mergeProperty(logger: Logger?, other: Config, property: KMutableProperty1<Config, String>): String {
         val v1 = property.get(this)
         val v2 = property.get(other)
         if (v1.isNotEmpty() && v2.isNotEmpty()) {
             logger?.warn("Duplicated property ${property.name}")
         }
-        if (v2.isNotEmpty()) {
-            property.set(this, v2)
-        }
+        return if (v2.isNotEmpty()) v2 else v1
     }
 
 }
--- buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
+++ buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
@@ -3,13 +3,14 @@ package com.cliqz.gradle
 import com.android.build.OutputFile
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.api.ApkVariantOutput
-import org.gradle.api.DefaultTask
+import com.android.build.gradle.api.ApplicationVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 @Suppress("unused")
 class CliqzPlugin: Plugin<Project> {
     override fun apply(project: Project) {
+        // TODO: Can this be moved to afterEvaluate too?
         val android = project.property("android") as AppExtension
         android.productFlavors.names.forEach {
             android.defaultConfig.apply {
@@ -20,22 +21,49 @@ class CliqzPlugin: Plugin<Project> {
             }
         }
 
-        val preBuild = project.property("preBuild") as DefaultTask
         val defaultVersionCode = android.defaultConfig.versionCode
-        preBuild.doLast {
+        project.afterEvaluate {
             android.applicationVariants.forEach { variant ->
-                val apiVersion = variant.productFlavors[0].versionCode
-                variant.outputs.forEach { output ->
-                    val abi = output.filters.find { it.filterType == OutputFile.ABI }
-                    abi?.let {
-                        val abiVersion = ABI_CODES[it.identifier]
-                        (output as ApkVariantOutput).versionCodeOverride =
-                                defaultVersionCode * 100 + apiVersion * 10 + abiVersion!!
-                    }
-                }
+                setVersionCode(defaultVersionCode, variant)
+                createCliqzConfigTasks(project, variant)
             }
         }
     }
+
+    private fun createCliqzConfigTasks(project: Project, variant: ApplicationVariant) {
+        val buildConfigProvider = variant.generateBuildConfigProvider
+        val javaCompileProvider = variant.javaCompileProvider
+        // val sourceOutdir = buildConfig.sourceOutputDir
+        val taskName = "generate${variant.name.capitalize()}CliqzConfig"
+        // val packageName = buildConfig.buildConfigPackageName
+        val builtType = variant.buildType.name
+        // This generate possible config names to look for in the json
+        val flavorNames = variant.productFlavors.fold(emptySet<String>()) { acc, productFlavor ->
+            acc + (listOf(productFlavor.name, "${productFlavor.name}${builtType.capitalize()}", builtType))
+        }
+
+        val taskProvider = project.tasks.register(taskName, CliqzConfigTask::class.java, buildConfigProvider, flavorNames)
+        taskProvider.configure {
+            it.dependsOn(buildConfigProvider)
+        }
+        javaCompileProvider.configure {
+            it.dependsOn(taskProvider)
+        }
+    }
+
+    private fun setVersionCode(defaultVersionCode: Int, variant: ApplicationVariant) {
+        val apiVersion = variant.productFlavors[0].versionCode
+        variant.outputs.forEach { output ->
+            val abi = output.filters.find { it.filterType == OutputFile.ABI }
+            abi?.let {
+                val abiVersion = ABI_CODES[it.identifier]
+                (output as ApkVariantOutput).versionCodeOverride =
+                        defaultVersionCode * 100 + apiVersion * 10 + abiVersion!!
+            }
+        }
+    }
+
+
 
     companion object {
         private val ABI_CODES = mapOf(
