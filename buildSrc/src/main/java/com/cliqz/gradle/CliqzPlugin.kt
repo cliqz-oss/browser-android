e38eb2c7a395a1f63bb863794310007fa644be32
Sharath <sharath@cliqz.com>
2019-07-04 13:24:01 +0200

    Fix Build Number not being passed to the Container (#144)

--- buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
+++ buildSrc/src/main/java/com/cliqz/gradle/CliqzPlugin.kt
@@ -56,7 +56,7 @@ class CliqzPlugin: Plugin<Project> {
     private fun setVersionCode(variant: ApplicationVariant) {
         val buildNumber = System.getenv("BUILD_NUMBER")?.toIntOrNull()
         if (buildNumber == null) return;
-        val versionCode = 110 + buildNumber
+        val versionCode = 150 + buildNumber
         val apiVersion = variant.productFlavors[0].versionCode
         variant.outputs.forEach { output ->
             val abi = output.filters.find { it.filterType == OutputFile.ABI }
