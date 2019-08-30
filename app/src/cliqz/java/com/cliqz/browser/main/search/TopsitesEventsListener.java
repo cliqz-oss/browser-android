e5a32e30491b9a9142cc19b99ec26292db709d18
Jayesh Solanki <jayeshsolanki93@gmail.com>
2019-08-30 15:24:13 +0200

    AB-2859 Simplify the deletion of topsites (#223)

--- app/src/cliqz/java/com/cliqz/browser/main/search/TopsitesEventsListener.java
+++ app/src/cliqz/java/com/cliqz/browser/main/search/TopsitesEventsListener.java
@@ -1,7 +1,5 @@
 package com.cliqz.browser.main.search;
 
-import android.os.SystemClock;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -16,11 +14,9 @@ import com.cliqz.browser.webview.Topsite;
  * @author Stefano Pacifici
  */
 class TopsitesEventsListener implements AdapterView.OnItemLongClickListener,
-        AdapterView.OnItemClickListener, View.OnTouchListener {
+        AdapterView.OnItemClickListener {
 
     private final Freshtab freshtab;
-    private float mLastEventRawX;
-    private float mLastEventRawY;
 
     TopsitesEventsListener(Freshtab freshtab) {
         this.freshtab = freshtab;
@@ -30,20 +26,8 @@ class TopsitesEventsListener implements AdapterView.OnItemLongClickListener,
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         final TopsitesAdapter adapter = TopsitesAdapter.class.cast(parent.getAdapter());
         if (adapter.getItemViewType(position) == TopsitesAdapter.TOPSITE_TYPE) {
-            freshtab.removeTopsitesOverlay.start(freshtab.topsitesGridView);
-            freshtab.contanier.requestDisallowInterceptTouchEvent(true);
-            // Just send an ACTION_DOWN for the picked view, this is part of the workaround to make
-            // topsites immediately movable after long pressing them
-            final long now = SystemClock.uptimeMillis();
-            final MotionEvent event = MotionEvent.obtain(now, now, MotionEvent.ACTION_DOWN,
-                    mLastEventRawX, mLastEventRawY, 0);
-            parent.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    freshtab.removeTopsitesOverlay.dispatchTouchEvent(event);
-                    event.recycle();
-                }
-            }, 100);
+            final Topsite topsite = (Topsite) adapter.getItem(position);
+            TopSitesContextMenu.showMenu(view, freshtab, topsite);
         }
         return true;
     }
@@ -56,22 +40,11 @@ class TopsitesEventsListener implements AdapterView.OnItemLongClickListener,
             return;
         }
         final Animation animation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
-                view.getX() + view.getWidth()/2, view.getY() + view.getHeight()/2);
+                view.getX() + view.getWidth() / 2f, view.getY() + view.getHeight() / 2f);
         animation.setDuration(200);
         final Topsite topsite = (Topsite) item;
         freshtab.bus.post(CliqzMessages.OpenLink.open(topsite.url, animation));
         freshtab.telemetry.sendTopsitesClickSignal(position, adapter.getDisplayedCount());
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        // Workaround to make topsites immediately movable after long pressing them
-        if (freshtab.removeTopsitesOverlay.isStarted()) {
-            freshtab.removeTopsitesOverlay.dispatchTouchEvent(event);
-            return true;
-        }
-        mLastEventRawX = event.getRawX();
-        mLastEventRawY = event.getRawY();
-        return false;
-    }
 }
