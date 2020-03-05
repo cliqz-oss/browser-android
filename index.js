dbf89ab25d73e45fab40e37420917989f5813afb
Stefano Pacifici <stefano.pacifici@gmail.com>
2020-03-05 17:56:53 +0100

    Patching extension telemetry

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ import { AppRegistry } from 'react-native';
 import setup from './setup';
 import React from 'react';
 import { startup, components } from 'browser-core';
+import events from 'browser-core/build/modules/core/events';
+import { openLink } from 'browser-core/build/modules/platform/browser-actions';
 
 // set app global for debugging
 const appStart = startup.then((app) => {
+  app.prefs.set('logger.telemetry.level', 'debug');
+  const mobileCardsBackground = app.modules['mobile-cards'].background;
+  mobileCardsBackground.actions.openLink = async function (url, selection) {
+    if (selection) {
+      await this.search.action('reportSelection', selection, { contextId: undefined });
+    }
+    openLink(url, selection.query);
+  }.bind(mobileCardsBackground);
+  events.sub('urlbar:blur', () => {
+    app.modules.search.background.actions.stopSearch({}, { contextId: undefined });
+  });
   global.app = app;
 });
 
